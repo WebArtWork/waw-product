403c95006f54b82bb348f18aa4808cdafdcb6616
Nika <129855721+nikiquickie@users.noreply.github.com>
2023-12-28 18:01:55 +0200

    Update product.api.js

--- product.api.js
+++ product.api.js
@@ -88,7 +88,9 @@ module.exports = async (waw) => {
 						.toLowerCase()
 						.replace(/[^a-z0-9]/g, "");
 				}
-				if (req.body.url) {
+				if (!req.body.url) {
+					req.body.url = null; 
+				} else {
 					while (await waw.Product.count({ url: req.body.url })) {
 						const url = req.body.url.split("_");
 						req.body.url =
