bcf65923425587608c7dbe75d1a0d39697ec603c
Nika <129855721+nikiquickie@users.noreply.github.com>
2024-07-11 23:30:20 +0300

    Update product.size.collection.js

--- product.size.collection.js
+++ product.size.collection.js
@@ -10,5 +10,11 @@ module.exports = async function (waw) {
 		}
 	});
 
+	Schema.methods.create = function (obj, user) {
+		this.name = obj.name;
+		this.quantity = obj.quantity;
+
+	};
+
 	return (waw.ProductSize = waw.mongoose.model("ProductSize", Schema));
 };
