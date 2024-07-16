4bd7e84d2c6a34e7c9035b5aa436431681044cd4
Nika <129855721+nikiquickie@users.noreply.github.com>
2024-07-16 23:42:26 +0300

    Update product.size.collection.js

--- product.size.collection.js
+++ product.size.collection.js
@@ -2,18 +2,13 @@ module.exports = async function (waw) {
 	const Schema = new waw.mongoose.Schema({
 		name: {
 			type: String,
-			required: true,
-		},
-		quantity: {
-			type: Number,
-			required: true,
+			required: true
 		}
 	});
 
 	Schema.methods.create = function (obj) {
 		this.name = obj.name;
-		this.quantity = obj.quantity;
 	};
 
-	return (waw.ProductSize = waw.mongoose.model("ProductSize", Schema));
+	return (waw.Productsize = waw.mongoose.model("Productsize", Schema));
 };
