242df3e324ef10f5fe24a74940cc5b894d364ddb
Nika <129855721+nikiquickie@users.noreply.github.com>
2024-07-16 23:41:21 +0300

    Update product.quantity.api.js

--- product.quantity.api.js
+++ product.quantity.api.js
@@ -1,47 +1,30 @@
 module.exports = async (waw) => {
-	const ensure = (req, res, next) => {
-		if (!req.user) {
-			req.body.sessionID = req.sessionID;
-		}
-		next();
-	};
-
 	waw.crud("productquantity", {
 		create: {
-			ensure: async (req, res, next) => {
-				if (!req.user) {
-					req.body.sessionID = req.sessionID;
-				}
-				const productSize = await waw.ProductSize.findById(req.body.size);
-				if (productSize) {
-					productSize.quantity = req.body.quantity;
-					await productSize.save();
-					res.send(true);
-				} else {
-					res.status(404).send("Product size not found");
-				}
-			},
+			ensure: waw.role("owner") 
 		},
 		get: {
-			ensure: waw.block,
+			ensure: waw.role("owner"),
+			query: () => {
+				return {};
+			},
+			populate: () => 'product size'
 		},
 		update: {
-			ensure: waw.block,
+			ensure: waw.role("owner"),
+			query: req => {
+				return {
+					_id: req.body._id
+				}
+			}
 		},
 		fetch: {
-			ensure: waw.block,
+			ensure: waw.role("owner"),
 		},
 		delete: {
-			ensure,
-			query: async (req) => {
-				const productSize = await waw.ProductSize.findById(req.body.size);
-				if (productSize) {
-					productSize.quantity = 0;
-					await productSize.save();
-					return true;
-				} else {
-					res.status(404).send("Product size not found");
-				}
+			ensure: waw.role("owner"),
+			query: (req) => {
+				return { _id: req.body._id };
 			},
 		},
 	});
