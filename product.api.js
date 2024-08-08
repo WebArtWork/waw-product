28956509e410155968bf2e44c5b295980d8e09ca
Nika <129855721+nikiquickie@users.noreply.github.com>
2024-08-08 21:38:22 +0300

    Update product.api.js

--- product.api.js
+++ product.api.js
@@ -188,7 +188,7 @@ module.exports = async (waw) => {
 
 					if (product.size) {
 						product.size.forEach(size => {
-							size.size._id = size.size.size._id.toString();
+							size.size._id = size.size._id.toString();
 						});
 					}
 				}
